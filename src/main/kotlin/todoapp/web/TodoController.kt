0c75c5e511b6df6ac56e90ec14d665955703c443
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-08-11 23:57:14 +0900

    csv 다운로드 기능 추가

--- src/main/kotlin/todoapp/web/TodoController.kt
+++ src/main/kotlin/todoapp/web/TodoController.kt
@@ -1,19 +1,80 @@
 package todoapp.web
 
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.View
+import org.springframework.web.servlet.ViewResolver
+import org.springframework.web.servlet.view.AbstractView
+import todoapp.commons.web.view.CommaSeparatedValuesView
+import todoapp.core.todo.application.TodoFind
+import todoapp.core.todo.domain.Todo
+import todoapp.web.convert.TodoToSpreadsheetConverter
+
+
 import todoapp.web.model.SiteProperties
+import java.net.http.HttpHeaders
+import java.util.*
 
 @Controller
-class TodoController (
+class TodoController(
     private val siteProperties: SiteProperties,
+    private val find: TodoFind
 ) {
     @ModelAttribute("site")
     fun siteProperties() = siteProperties
 
     @RequestMapping("/todos")
     fun todos() {
+    }
+
+    @RequestMapping(value = ["/todos"], produces = ["text/csv"])
+    fun downloadTodos(model: Model) {
+        model.addAttribute(TodoToSpreadsheetConverter().convert(find.all()))
+    }
+
+    class TodoCsvViewResolver : ViewResolver {
+        override fun resolveViewName(viewName: String, locale: Locale): View? {
+            return if (viewName == "todos") {
+                CommaSeparatedValuesView()
+            } else {
+                null
+            }
+        }
+    }
+
+    class TodoCsvView : AbstractView(), View {
+
+        private val log = LoggerFactory.getLogger(javaClass)
+
+        init {
+            contentType = "text/csv"
+        }
+
+        override fun generatesDownloadContent(): Boolean {
+            return true
+        }
+
+        override fun renderMergedOutputModel(
+            model: MutableMap<String, Any>,
+            request: HttpServletRequest,
+            response: HttpServletResponse
+        ) {
+            log.info("render model as csv content")
+
+            response.addHeader(org.springframework.http.HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"todos.csv\"")
+            response.writer.println("id,title,completed")
+            val todos = model.getOrDefault("todos", emptyList<Todo>()) as List<Todo>
+            todos.forEach {
+                val line = "${it.id}, ${it.title}, ${it.isCompleted}"
+                response.writer.println(line)
+            }
+
+            response.flushBuffer()
+        }
     }
 }
--- src/main/kotlin/todoapp/web/TodoRestController.kt
+++ src/main/kotlin/todoapp/web/TodoRestController.kt
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,15 +15,20 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import todoapp.core.todo.application.TodoCleanup
 import todoapp.core.todo.application.TodoFind
+import todoapp.core.todo.application.TodoModification
 import todoapp.core.todo.application.TodoRegistry
 import todoapp.core.todo.domain.Todo
+import todoapp.core.todo.domain.TodoId
 
 @RestController
 @RequestMapping(path = ["/api/todos"])
 class TodoRestController(
     private val find: TodoFind,
     private val registry: TodoRegistry,
+    private val modification: TodoModification,
+    private val cleanup: TodoCleanup
 ) {
 
     val log: Logger = LoggerFactory.getLogger(javaClass)
@@ -34,19 +40,31 @@ class TodoRestController(
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody @Valid command: CreateTodoCommand) {
+    fun create(@RequestBody @Valid command: WriteTodoCommand) {
         log.info("request: $command")
 
         registry.register(command.title)
     }
 
     @PutMapping("/{id}")
-    fun update(@PathVariable("id") id: String) {
+    fun update(@PathVariable("id") id: String, @RequestBody @Valid command: WriteTodoCommand) {
         log.info("request id: $id")
+
+        modification.modify(TodoId.of(id), command.title, false)
     }
 
-    data class CreateTodoCommand(
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable("id") id: String) {
+        log.info("request id: $id")
+
+        cleanup.clear(TodoId.of(id))
+    }
+
+    data class WriteTodoCommand(
         @field:Size(min = 4, max = 140)
         val title: String,
-    )
+        val completed: Boolean
+    ){
+
+}
 }
